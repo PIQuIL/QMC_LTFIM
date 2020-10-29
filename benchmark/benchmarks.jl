7bd9b7b2689c01f7da148d4e9737a397ead930fc
Ejaaz Merali <emerali@users.noreply.github.com>
2020-10-29 13:57:00 -0400

    More consistent benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,14 +9,11 @@ SUITE = BenchmarkGroup()
 SUITE["probability_vector"] = BenchmarkGroup()
 
 for n in 10:10:100
-    p = rand(1:10, n)
-
-    p_vec = ProbabilityVector(p)
-    p_heap = ProbabilityHeap(p)
-
     SUITE["probability_vector"][n] = BenchmarkGroup()
-    SUITE["probability_vector"][n]["vector"] = @benchmarkable rand($p_vec)
-    SUITE["probability_vector"][n]["heap"] = @benchmarkable rand($p_heap)
+    SUITE["probability_vector"][n]["vector"] =
+        @benchmarkable rand(p_vec) setup=(p_vec = ProbabilityVector(rand(1:10, $n)))
+    SUITE["probability_vector"][n]["heap"] =
+        @benchmarkable rand(p_heap) setup=(p_heap = ProbabilityHeap(rand(1:10, $n)))
 end
 
 
@@ -34,17 +31,25 @@ for M = 200:200:1000
 
     for (H, gs) in [(HT, "TFIM_groundstate"), (HL, "LTFIM_groundstate")]
         SUITE[gs][M] = BenchmarkGroup()
-        groundstate = BinaryGroundState(H, M)
 
         SUITE[gs][M]["diagonal_update"] =
-            @benchmarkable QMC.diagonal_update!($groundstate, $H)
-        SUITE[gs][M]["linked_list_update"] =
-            @benchmarkable QMC.link_list_update!($groundstate, $H)
-        SUITE[gs][M]["cluster_update"] =
-            @benchmarkable(QMC.cluster_update!(lsize, $groundstate, $H),
-                        setup=(lsize = QMC.link_list_update!($groundstate, $H)))
+            @benchmarkable(QMC.diagonal_update!(groundstate, $H),
+                           setup=(groundstate = BinaryGroundState($H, $M)))
 
-        SUITE[gs][M]["mc_step"] = @benchmarkable QMC.mc_step!($groundstate, $H)
+        SUITE[gs][M]["linked_list_update"] =
+            @benchmarkable(QMC.link_list_update!(groundstate, $H),
+                           setup=(groundstate = BinaryGroundState($H, $M);
+                                  QMC.diagonal_update!(groundstate, $H)))
+
+        SUITE[gs][M]["cluster_update"] =
+            @benchmarkable(QMC.cluster_update!(lsize, groundstate, $H),
+                           setup=(groundstate = BinaryGroundState($H, $M);
+                                  QMC.diagonal_update!(groundstate, $H);
+                                  lsize = QMC.link_list_update!(groundstate, $H)))
+
+        SUITE[gs][M]["mc_step"] =
+            @benchmarkable(QMC.mc_step!(groundstate, $H),
+                           setup=(groundstate = BinaryGroundState($H, $M)))
     end
 
     ###########################################################################
@@ -53,16 +58,24 @@ for M = 200:200:1000
     for (H, ts) in [(HT, "TFIM_thermalstate"), (HL, "LTFIM_thermalstate")]
         SUITE[ts][M] = BenchmarkGroup()
         beta = 10.0
-        thermalstate = BinaryThermalState(H, M)
 
         SUITE[ts][M]["diagonal_update"] =
-            @benchmarkable QMC.diagonal_update_beta!($thermalstate, $H, $beta)
-        SUITE[ts][M]["linked_list_update"] =
-            @benchmarkable QMC.link_list_update_beta!($thermalstate, $H)
-        SUITE[ts][M]["cluster_update"] =
-            @benchmarkable(QMC.cluster_update!(lsize, $thermalstate, $H),
-                        setup=(lsize = QMC.link_list_update_beta!($thermalstate, $H)))
+            @benchmarkable(QMC.diagonal_update_beta!(thermalstate, $H, $beta),
+                           setup=(thermalstate = BinaryThermalState($H, $M)))
 
-        SUITE[ts][M]["mc_step"] = @benchmarkable QMC.mc_step_beta!($thermalstate, $H, $beta)
+        SUITE[ts][M]["linked_list_update"] =
+            @benchmarkable(QMC.link_list_update_beta!(thermalstate, $H),
+                           setup=(thermalstate = BinaryThermalState($H, $M);
+                                  QMC.diagonal_update_beta!(thermalstate, $H, $beta)))
+
+        SUITE[ts][M]["cluster_update"] =
+            @benchmarkable(QMC.cluster_update!(lsize, thermalstate, $H),
+                           setup=(thermalstate = BinaryThermalState($H, $M);
+                                  QMC.diagonal_update_beta!(thermalstate, $H, $beta);
+                                  lsize = QMC.link_list_update_beta!(thermalstate, $H)))
+
+        SUITE[ts][M]["mc_step"] =
+            @benchmarkable(QMC.mc_step_beta!(thermalstate, $H, $beta),
+                           setup=(thermalstate = BinaryThermalState($H, $M)))
     end
 end
