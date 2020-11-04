8e82f7d5277cf4ae5d57f8e41efa7670de73cea3
Ejaaz Merali <emerali@users.noreply.github.com>
2020-11-04 12:27:31 -0500

    Speed up accessing weights through OperatorSampler

--- src/op_sampler.jl
+++ src/op_sampler.jl
@@ -3,26 +3,72 @@ abstract type AbstractOperatorSampler{K, T, P <: AbstractProbabilityVector{T}} e
 firstindex(::AbstractOperatorSampler) = 1
 lastindex(os::AbstractOperatorSampler) = length(os)
 
-getweight(os::AbstractOperatorSampler{K}, op::NTuple{K, Int}) where K = haskey(os.op_indices, op) ? getweight(os.pvec, os.op_indices[op]) : 0
+function getweight(os::AbstractOperatorSampler{K, T}, op::NTuple{K, Int}) where {K, T}
+    # idx = get(os.op_indices, op, 0)
+    idx = os.op_indices[conv_op_to_idx(op, os.strides, os.shifts)]
+    # idx = get(os.op_indices, conv_op_to_idx(op, os.strides, os.shifts), 0)
+    if iszero(idx)
+        return zero(T)
+    else
+        return getweight(os.pvec, idx)
+    end
+end
+
+
+function getlogweight(os::AbstractOperatorSampler{K, T}, op::NTuple{K, Int}) where {K, T}
+    # idx = get(os.op_indices, op, 0)
+    idx = os.op_indices[conv_op_to_idx(op, os.strides, os.shifts)]
+    # idx = get(os.op_indices, conv_op_to_idx(op, os.strides, os.shifts), 0)
+    if iszero(idx)
+        return zero(T)
+    else
+        return getlogweight(os.pvec, idx)
+    end
+end
 
 
 struct OperatorSampler{K, T, P} <: AbstractOperatorSampler{K, T, P}
     operators::Vector{NTuple{K, Int}}
     pvec::P
-    op_indices::Dict{NTuple{K, Int}, Int}
+    op_indices::Vector{Int}
+    strides::NTuple{K, Int}
+    shifts::NTuple{K, Int}
 end
 
+
+function conv_op_to_idx(op::NTuple{K, Int}, strides::NTuple{K, Int}, shifts::NTuple{K, Int}) where K
+    idx = 0
+    @inbounds for i in reverse(eachindex(op))
+        idx += (op[i] - shifts[i])
+        idx *= strides[i]
+    end
+    return idx + 1
+end
 
 function OperatorSampler(operators::Vector{NTuple{K, Int}}, p::Vector{T}) where {K, T <: Real}
     @assert length(operators) == length(p) "Given vectors must have the same length!"
     pvec = probability_vector(p)
-    op_indices = Dict{NTuple{K, Int}, Int}(op => i for (i, op) in enumerate(operators))
-    return OperatorSampler{K, T, typeof(pvec)}(operators, pvec, op_indices)
+
+    axs = ntuple(i -> extrema(x->x[i], operators), K)
+    strides = tuple(1, [max - min + 1 for (min, max) in axs[1:end-1]]...)
+    shifts = tuple([min for (min, _) in axs]...)
+
+    l = conv_op_to_idx(tuple([max for (_, max) in axs]...), strides, shifts)
+
+    op_indices = zeros(Int, l)
+    for (i, op) in enumerate(operators)
+        idx = conv_op_to_idx(op, strides, shifts)
+        op_indices[idx] = i
+    end
+    return OperatorSampler{K, T, typeof(pvec)}(operators, pvec, op_indices, strides, shifts)
 end
 rand(rng::AbstractRNG, os::OperatorSampler{K}) where K = @inbounds os.operators[rand(rng, os.pvec)]
 @inline length(os::OperatorSampler) = length(os.operators)
 
+###############################################################################
 
+# Leave HierarchicalOperatorSampler using dictionaries for now just to have an
+#  easily accessible "ground-truth" comparison.
 
 
 function cluster_probs_vec(operators::Vector{NTuple{K, Int}}, p::AbstractVector{T}) where {K, T <: Real}
@@ -77,10 +123,11 @@ function rand(rng::AbstractRNG, os::HierarchicalOperatorSampler{K})::NTuple{K, I
     return @inbounds ops_list[l]
 end
 
-function getweight(os::HierarchicalOperatorSampler{K}, op::NTuple{K, Int}) where K
-    if !haskey(os.op_indices, op)
-        return 0
+function getweight(os::HierarchicalOperatorSampler{K, T}, op::NTuple{K, Int}) where {K, T}
+    idx = get(os.op_indices, op, 0)
+    if iszero(idx)
+        return zero(T)
+    else
+        return getweight(os.pvec, idx) / length(os.operator_bins[idx])
     end
-    idx = os.op_indices[op]
-    return getweight(os.pvec, idx) / length(os.operator_bins[idx])
 end
