45947f08bac5bc5f98b9cd41589101c37e415d98
Ejaaz Merali <emerali@users.noreply.github.com>
2020-11-18 19:44:29 -0500

    More consistent benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,7 +2,6 @@ using BenchmarkTools
 using QMC
 
 using Random
-Random.seed!(1234)
 
 SUITE = BenchmarkGroup()
 
@@ -11,11 +10,11 @@ SUITE["probability_vector"] = BenchmarkGroup()
 for n in 10:10:100
     SUITE["probability_vector"][n] = BenchmarkGroup()
     SUITE["probability_vector"][n]["vector"] =
-        @benchmarkable rand(p_vec) setup=(p_vec = ProbabilityVector(rand(1:10, $n)))
+        @benchmarkable rand(rng, p_vec) setup=(rng = MersenneTwister(1234); p_vec = ProbabilityVector(rand(rng, 1:10, $n)))
     SUITE["probability_vector"][n]["heap"] =
-        @benchmarkable rand(p_heap) setup=(p_heap = ProbabilityHeap(rand(1:10, $n)))
+        @benchmarkable rand(rng, p_heap) setup=(rng = MersenneTwister(1234); p_heap = ProbabilityHeap(rand(rng, 1:10, $n)))
     SUITE["probability_vector"][n]["alias"] =
-        @benchmarkable rand(p_alias) setup=(p_alias = ProbabilityAlias(rand(1:10, $n)))
+        @benchmarkable rand(rng, p_alias) setup=(rng = MersenneTwister(1234); p_alias = ProbabilityAlias(rand(rng, 1:10, $n)))
 end
 
 
@@ -35,23 +34,27 @@ for M = 200:200:1000
         SUITE[gs][M] = BenchmarkGroup()
 
         SUITE[gs][M]["full_diagonal_update"] =
-            @benchmarkable(QMC.full_diagonal_update!(groundstate, $H),
-                           setup=(groundstate = BinaryGroundState($H, $M)))
+            @benchmarkable(QMC.full_diagonal_update!(rng, groundstate, $H),
+                           setup=(rng = MersenneTwister(1234);
+                                  groundstate = BinaryGroundState($H, $M)))
 
         SUITE[gs][M]["linked_list_update"] =
-            @benchmarkable(QMC.link_list_update!(groundstate, $H),
-                           setup=(groundstate = BinaryGroundState($H, $M);
-                                  QMC.full_diagonal_update!(groundstate, $H)))
+            @benchmarkable(QMC.link_list_update!(rng, groundstate, $H),
+                           setup=(rng = MersenneTwister(1234);
+                                  groundstate = BinaryGroundState($H, $M);
+                                  QMC.full_diagonal_update!(rng, groundstate, $H)))
 
         SUITE[gs][M]["cluster_update"] =
-            @benchmarkable(QMC.cluster_update!(lsize, groundstate, $H),
-                           setup=(groundstate = BinaryGroundState($H, $M);
-                                  QMC.full_diagonal_update!(groundstate, $H);
-                                  lsize = QMC.link_list_update!(groundstate, $H)))
+            @benchmarkable(QMC.cluster_update!(rng, lsize, groundstate, $H),
+                           setup=(rng = MersenneTwister(1234);
+                                  groundstate = BinaryGroundState($H, $M);
+                                  QMC.full_diagonal_update!(rng, groundstate, $H);
+                                  lsize = QMC.link_list_update!(rng, groundstate, $H)))
 
         SUITE[gs][M]["mc_step"] =
-            @benchmarkable(QMC.mc_step!(groundstate, $H),
-                           setup=(groundstate = BinaryGroundState($H, $M)))
+            @benchmarkable(QMC.mc_step!(rng, groundstate, $H),
+                           setup=(rng = MersenneTwister(1234);
+                                  groundstate = BinaryGroundState($H, $M)))
     end
 
     ###########################################################################
@@ -62,22 +65,26 @@ for M = 200:200:1000
         beta = 10.0
 
         SUITE[ts][M]["full_diagonal_update"] =
-            @benchmarkable(QMC.full_diagonal_update_beta!(thermalstate, $H, $beta),
-                           setup=(thermalstate = BinaryThermalState($H, $M)))
+            @benchmarkable(QMC.full_diagonal_update_beta!(rng, thermalstate, $H, $beta),
+                           setup=(rng = MersenneTwister(1234);
+                                  thermalstate = BinaryThermalState($H, $M)))
 
         SUITE[ts][M]["linked_list_update"] =
-            @benchmarkable(QMC.link_list_update!(thermalstate, $H),
-                           setup=(thermalstate = BinaryThermalState($H, $M);
-                                  QMC.full_diagonal_update_beta!(thermalstate, $H, $beta)))
+            @benchmarkable(QMC.link_list_update!(rng, thermalstate, $H),
+                           setup=(rng = MersenneTwister(1234);
+                                  thermalstate = BinaryThermalState($H, $M);
+                                  QMC.full_diagonal_update_beta!(rng, thermalstate, $H, $beta)))
 
         SUITE[ts][M]["cluster_update"] =
-            @benchmarkable(QMC.cluster_update!(lsize, thermalstate, $H),
-                           setup=(thermalstate = BinaryThermalState($H, $M);
-                                  QMC.full_diagonal_update_beta!(thermalstate, $H, $beta);
-                                  lsize = QMC.link_list_update!(thermalstate, $H)))
+            @benchmarkable(QMC.cluster_update!(rng, lsize, thermalstate, $H),
+                           setup=(rng = MersenneTwister(1234);
+                                  thermalstate = BinaryThermalState($H, $M);
+                                  QMC.full_diagonal_update_beta!(rng, thermalstate, $H, $beta);
+                                  lsize = QMC.link_list_update!(rng, thermalstate, $H)))
 
         SUITE[ts][M]["mc_step"] =
-            @benchmarkable(QMC.mc_step_beta!(thermalstate, $H, $beta),
-                           setup=(thermalstate = BinaryThermalState($H, $M)))
+            @benchmarkable(QMC.mc_step_beta!(rng, thermalstate, $H, $beta),
+                           setup=(rng = MersenneTwister(1234);
+                                  thermalstate = BinaryThermalState($H, $M)))
     end
 end
