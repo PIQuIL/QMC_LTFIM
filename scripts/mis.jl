d40824b3fb1eb8593ba9be1edf819cb9bb194c8d
Roger-luo <rogerluo.rl18@gmail.com>
2021-01-13 17:30:55 -0500

    add distance matrix

--- scripts/mis.jl
+++ scripts/mis.jl
@@ -4,6 +4,8 @@ using DrWatson
 using QMC
 
 using RydbergEmulator
+using LinearAlgebra
+using LightGraphs
 
 function load_instance(n, ff)
     L = ceil(Int, sqrt(n/ff))
@@ -16,5 +18,15 @@ function load_instance(n, ff)
     end
 end
 
+function distance_matrix(graph)
+    V = zeros(20, 20)
+    for e in edges(graph)
+        V[e.dst, e.src] = RydbergEmulator.distance(atoms[e.dst], atoms[e.src])
+    end
+    return UpperTriangular(V')
+end
+
+
 atoms = load_instance(20, 0.8)
 graph = unit_disk_graph(atoms, 1.5)
+V = distance_matrix(graph)
