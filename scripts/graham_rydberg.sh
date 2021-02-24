be8f73ba2a922e25b9bbb5fc9e5ed59f7d6dd75e
Ejaaz Merali <emerali@users.noreply.github.com>
2021-02-24 11:04:55 -0500

    minor

--- scripts/graham_rydberg.sh
+++ scripts/graham_rydberg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for delta in $(seq 1.0 0.01 1.3)
 do
-  X="omega=1.0,Rb=1.2,delta=${delta}"
+  X="delta=${delta},omega=1.0,Rb=1.2"
   sbatch -J "$X" --export="$X" submit_rydberg
 done
 
