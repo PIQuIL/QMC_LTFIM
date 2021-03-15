0f3d7a4b61c5d9c27f2cbd680893908ab4d79e9f
Ejaaz Merali <emerali@users.noreply.github.com>
2021-03-15 17:08:32 -0400

    minor

--- scripts/graham_rydberg.sh
+++ scripts/graham_rydberg.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 for M in $(seq 2000 2000 10000)
 do
-  for p in $(seq 0.0 0.1 0.1)
+  for p in $(seq 0.4 0.1 0.5)
   do
     for delta in $(seq 1.01 0.01 1.2)
     do
       X="delta=$delta,M=$M,p=$p"
       sbatch -J "p=$p,d=$delta,M=$M" --export="$X" submit_rydberg
     done
+    sleep 2s
   done
 done
 
