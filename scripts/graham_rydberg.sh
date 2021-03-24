b8ab6f3ebc6fedb4e3c6f93fdde29f51c0c45e71
Ejaaz Merali <emerali@users.noreply.github.com>
2021-03-24 13:15:29 -0400

    Add continuation script

--- scripts/graham_rydberg.sh
+++ scripts/graham_rydberg.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-for delta in $(seq 1.01 0.01 1.2)
+for delta in $(seq 1.02 0.01 1.2)
 do
-  X="delta=$delta,nY=$nY"
-  sbatch -J "Ryd_Disord2Checkerboard" --export="$X" submit_rydberg
-  sleep 0.5s
+  X="delta=$delta"
+  #echo $X
+  sbatch -J "Ryd_D2C_contd" --export="$X" continue_rydberg
+  sleep 1s
 done
+
