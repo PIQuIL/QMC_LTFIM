51065970922189ae8a328326a620c0fea9bfb1f7
Ejaaz Merali <emerali@users.noreply.github.com>
2021-04-07 11:30:57 -0400

    minor

--- scripts/graham_rydberg.sh
+++ scripts/graham_rydberg.sh
@@ -3,11 +3,13 @@ for i in $(seq 0 9)
 do
   delta_i=$(echo "1.01 + ($i*0.02)" | bc)
   delta_f=$(echo "$delta_i + 0.01" | bc)
-  deltas=$(echo "$delta_i\n$delta_f")
+  deltas=$(echo -E "$delta_i\n$delta_f")
   for nY in $(seq 8 8 32)
   do
     X="nY=$nY,deltas=$deltas"
-    sbatch -J "nY=$nY_δ=$delta_i-$delta_f" --export="$X" submit_rydberg
+    echo $X
+    sbatch -J "$nY,$delta_i-$delta_f" --export="$X" submit_rydberg
     sleep 0.5s
   done
 done
+
