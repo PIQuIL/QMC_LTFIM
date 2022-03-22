26d9bce2a495b797832571494220971802d6aa6a
Ejaaz Merali <emerali@users.noreply.github.com>
2022-03-22 16:09:28 -0400

    forgot to push diagnostics folder

---
+++ src/diagnostics/runstats.jl
@@ -0,0 +1,32 @@
+using OnlineStats
+
+
+abstract type AbstractRunStats; end
+
+struct NoStats <: AbstractRunStats; end
+@inline OnlineStats.fit!(R::NoStats, ::Symbol, val) = R
+
+
+struct RunStats{T <: Real} <: AbstractRunStats
+    diag_update_fails::Variance{T}
+    cluster_update_accept::Variance{T}
+    cluster_count::Variance{T}
+    cluster_sizes::Variance{T}
+
+    RunStats{T}() where T = new{T}(Variance(T), Variance(T), Variance(T), Variance(T))
+end
+RunStats() = RunStats{Float64}()
+@inline OnlineStats.fit!(R::RunStats, field::Symbol, val) = (fit!(getproperty(R, field), val); R)
+
+
+struct RunStatsHistogram{T <: Real, R <: StepRangeLen} <: AbstractRunStats
+    diag_update_fails::KHist{T}
+    cluster_update_accept::Hist{T, R}
+    cluster_count::KHist{T}
+    cluster_sizes::KHist{T}
+
+    RunStatsHistogram{T}(size::Int) where T =
+        new{T, typeof(0:(1/size):1)}(KHist(size, T), Hist(0:(1/size):1, T), KHist(size, T), KHist(size, T))
+end
+RunStatsHistogram(size::Int) = RunStatsHistogram{Float64}(size::Int)
+@inline OnlineStats.fit!(R::RunStatsHistogram, field::Symbol, val) = (fit!(getproperty(R, field), val); R)
